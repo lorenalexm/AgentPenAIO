9e68c9e54ed61f88abc80faefa82d425a9717069
Alex Loren <lorenalexm@gmail.com>
2023-09-28 14:10:46 -0700

    Cleanup of Vapor template

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,10 +5,4 @@ func routes(_ app: Application) throws {
     app.get { req async throws in
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
 }
