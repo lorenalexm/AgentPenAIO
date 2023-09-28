232e1b0a86874830ce91108500592e6f38581cc1
Alex Loren <lorenalexm@gmail.com>
2023-09-28 13:58:53 -0700

    Updated Swift packages

--- Package.swift
+++ Package.swift
@@ -7,14 +7,13 @@ let package = Package(
        .macOS(.v13)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
-        // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
-        // 🐘 Fluent driver for Postgres.
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
-        // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.2.2"),
+        .package(url: "https://github.com/vapor/queues.git", from: "1.13.0"),
+        .package(url: "https://github.com/vapor/queues-redis-driver.git", from: "1.1.1"),
     ],
     targets: [
         .executableTarget(
@@ -23,12 +22,15 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Leaf", package: "leaf"),
+                .product(name: "JWT", package: "jwt"),
+                .product(name: "QueuesRedisDriver", package: "queues-redis-driver"),
                 .product(name: "Vapor", package: "vapor"),
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
+            .product(name: "XCTQueues", package: "queues"),
 
             // Workaround for https://github.com/apple/swift-package-manager/issues/6940
             .product(name: "Vapor", package: "vapor"),
