5f3e54c256d669f68e72a3e7ce2bc7761c026f8c
Alex Loren <lorenalexm@gmail.com>
2023-12-07 15:37:14 -0800

    Removed JWT package, added Firebase JWT package

--- Package.swift
+++ Package.swift
@@ -13,22 +13,22 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.5.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "4.2.2"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.13.0"),
         .package(url: "https://github.com/vapor/queues-redis-driver.git", from: "1.1.1"),
+        .package(url: "https://github.com/emvakar/vapor-firebase-jwt-middleware.git", branch: "master"),
         .package(url: "https://github.com/vadymmarkov/Fakery", from: "5.1.0")
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
+                .product(name: "Vapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Leaf", package: "leaf"),
-                .product(name: "JWT", package: "jwt"),
                 .product(name: "QueuesRedisDriver", package: "queues-redis-driver"),
-                .product(name: "Vapor", package: "vapor"),
+                .product(name: "FirebaseJWTMiddleware", package: "vapor-firebase-jwt-middleware"),
             ]
         ),
         .executableTarget(name: "Run",
